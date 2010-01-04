f52d5f7e945004a1df12ba3839bfaaccf5839994
flyfloh <flyfloh@larry.(none)>
2010-01-04 23:47:28 +0100

    refactored helper methods implemented logo display on dashboard

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,19 +1,19 @@
 module CompaniesHelper
 
-  def company_logo
+  def company_logo(company_logo)
     content_tag :div, :class => "company_logo" do
-      if @company.company_logo
-        image_tag(company_logo_path(@company.company_logo), :alt => "Company Logo")
+      if company_logo
+        image_tag(company_logo_path(company_logo), :alt => "Company Logo")
       else
         image_tag("icons/user_suit.png", :alt => "Dummy Company Logo")
       end
     end
   end
 
-  def update_company_logo_link
+  def update_company_logo_link(company_logo)
       content_tag :li do
-        if @company.company_logo
-          link_to "Update Company Logo", edit_company_logo_path(@company.company_logo)
+        if company_logo
+          link_to "Update Company Logo", edit_company_logo_path(company_logo)
         else
           link_to "Update Company Logo", new_company_logo_path
         end
--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -1,2 +1,6 @@
 module HomepageHelper
+
+  def display_company_logo(company)
+    image_tag company_logo_path(company.company_logo), :alt => "Company Logo", :style => "float:right;" if company.company_logo
+  end
 end
